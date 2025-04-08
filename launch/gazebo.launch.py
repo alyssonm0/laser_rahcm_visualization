61b4532e562f21bd8bdb08003c45b4a42ab2bfdc
Alysson <alyssommartim@gmail.com>
2025-04-08 14:33:51 -0300

    chico mendes com formas geometricas simples sem xacro

---
+++ launch/gazebo.launch.py
@@ -0,0 +1,59 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = "chico"
+    urdf_file_name = "chico_basic.urdf.xacro"
+
+    package_path = get_package_share_directory(package_name)
+    urdf_path = os.path.join(package_path, "urdf", urdf_file_name)
+
+    robot_description = Command([
+        "xacro ", urdf_path
+    ])
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("gazebo_ros"), "launch", "gazebo.launch.py")
+        )
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "chico", "-topic", "/robot_description"],
+        output="screen",
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_sim_time", default_value="true", description="Use simulation time"
+        ),
+        gazebo,
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{"robot_description": robot_description}],
+            output="screen",
+        ),
+        spawn_entity,
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen'
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        )
+
+    ])
